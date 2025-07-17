4d111c9c1c8275c3e590bef5d7fffeb948c84eec
indumathi-ravi <indumathi123indumathi@gmail.com>
2025-07-17 11:47:54 +0530

    update db

--- src/helper/db.ts
+++ src/helper/db.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "ZAD-Products-Admin",
-  password: process.env.DB_PASSWORD || "1234",
-  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export const executeQuery = async (
